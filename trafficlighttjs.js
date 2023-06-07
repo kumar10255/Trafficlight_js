0036cac33519a64803fd8cc5a796aa6067fd99e1
kumar <kkumar10255@gmail.com>
2023-06-07 12:46:13 +0530

    trafficlight

---
+++ trafficlighttjs.js
@@ -0,0 +1,27 @@
+
+function stopbutton() {
+    document.getElementById("stopButton").style.backgroundColor = "#cf1124";
+    document.getElementById("stopLight").style.backgroundColor = "#cf1124";
+    document.getElementById("readyButton").style.backgroundColor = "#1f1d41";
+    document.getElementById("readyLight").style.backgroundColor = "#4b5069";
+    document.getElementById("goButton").style.backgroundColor = "#1f1d41";
+    document.getElementById("goLight").style.backgroundColor = "#4b5069";
+}
+
+function readybutton() {
+    document.getElementById("stopButton").style.backgroundColor = "#1f1d41";
+    document.getElementById("stopLight").style.backgroundColor = "#4b5069";
+    document.getElementById("readyButton").style.backgroundColor = "#f7c948";
+    document.getElementById("readyLight").style.backgroundColor = "#f7c948";
+    document.getElementById("goButton").style.backgroundColor = "#1f1d41";
+    document.getElementById("goLight").style.backgroundColor = "#4b5069";
+}
+
+function golight() {
+    document.getElementById("stopButton").style.backgroundColor = "#1f1d41";
+    document.getElementById("stopLight").style.backgroundColor = "#4b5069";
+    document.getElementById("readyButton").style.backgroundColor = "#1f1d41";
+    document.getElementById("readyLight").style.backgroundColor = "#4b5069";
+    document.getElementById("goButton").style.backgroundColor = "#199473";
+    document.getElementById("goLight").style.backgroundColor = "#199473";
+}
